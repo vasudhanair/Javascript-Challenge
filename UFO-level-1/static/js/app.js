af980a7335e2aedac0bc70a9b6c7d94ab07ea4e3
SSN <vasudhanair@gmail.com>
2020-11-21 09:41:59 -0500

    Changes made to the app.js file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,5 +9,12 @@ tableData.forEach(function(ufoSighting){
     console.log(ufoSighting);
     // Append one table row `tr` for each UFO Sighting object
     var row = tbody.append("tr"); 
-}
-)
+    // Object.entries to console.log used for each UFO sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+
